0142dbdde600011cfe21f7c450e7d85bb3fa7d15
sergio <ssergiosantos088@gmail.com>
2025-04-30 16:02:28 -0300

    começando o X

--- X/server.js
+++ X/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const path = require('path')
 
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'homepage.html'))
+})
 
 app.listen(3000, ()=>{
     console.log('server aberto')
